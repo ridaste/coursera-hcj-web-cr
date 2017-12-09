296b531667b997fcb482616d6528925d2661f6d3
Chris Ritter <bayreuth@me.com>
2017-12-10 09:34:03 +1100

    module 5 first submission commit

--- module5-solution/js/ajax-utils.js
+++ module5-solution/js/ajax-utils.js
@@ -8,27 +8,27 @@ var ajaxUtils = {};
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
-  } 
+  }
   else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
+  }
   else {
     global.alert("Ajax is not supported!");
-    return(null); 
+    return(null);
   }
 }
 
 
 // Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
+ajaxUtils.sendGetRequest =
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
+    request.onreadystatechange =
+      function() {
+        handleResponse(request,
                        responseHandler,
-                       isJsonResponse); 
+                       isJsonResponse);
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
@@ -64,4 +64,3 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
-
